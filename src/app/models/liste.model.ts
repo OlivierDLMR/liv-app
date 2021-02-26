c6aaa3679d09d07c6cefc152f2430034e09d6ab6
Gwerrhod <abaillot.bnpparibas@gmail.com>
2021-02-26 15:06:09 +0100

    models

---
+++ src/app/models/liste.model.ts
@@ -0,0 +1,38 @@
+export interface Liste {
+  id: number;
+  titre: string;
+  dateCreation: Date;
+  dateModification: Date;
+  suivis: Array<Suivi>;
+}
+
+export interface Suivi {
+  id: number;
+  preview: Preview;
+  statut: Statut;
+  noteUser: number;
+  saisonEnCours: number;
+  dernierEpisodeVu;
+}
+
+export interface Preview {
+  id: number;
+  idMovieDb: string;
+  titre: string;
+  image: string;
+  dateCreation: Date;
+  overview: string;
+  typePreview: TypePreview;
+}
+
+export enum Statut {
+  onGoing = 'EN_COURS',
+  notSeen = 'PAS_VU',
+  finished = 'FINI',
+  abort = 'ABANDONNE',
+}
+
+export enum TypePreview {
+  FILM = 'FILM',
+  SERIE = 'SERIE',
+}
