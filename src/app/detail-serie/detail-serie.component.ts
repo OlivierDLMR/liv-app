dc22e71a67438da4e91d9c8c66f144db5ff75b62
OlivierDLMR <odlmr@yahoo.fr>
2021-03-01 15:31:37 +0100

    ajout component detail-serie + recherche serie OK

---
+++ src/app/detail-serie/detail-serie.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SerieService } from '../Shared/services/serie.service';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail-serie.component.html',
+  styleUrls: ['./detail-serie.component.scss']
+})
+export class DetailSerieComponent implements OnInit {
+  /*
+    injecter un objet de la class ActivatedRoute
+    permet de récupérer le paramètre id de l'url
+    ** avec .snapshot.params.id
+  */
+  serieId: number;
+  type: string;
+  serie;
+  urlYTSerie:any;
+  constructor(
+    private route: ActivatedRoute,
+    private serieService: SerieService,
+    private router: Router,
+    private sanitizer: DomSanitizer
+  ) { }
+
+  ngOnInit(): void {
+    // 1 recuperer l'ID du film ET le type ('serie', ou 'results')
+    this.serieId = this.route.snapshot.params.id;
+    this.type = this.route.snapshot.params.type;
+
+
+    if (this.type == 'series') {
+      // 2 récupérer les informations de la serie
+      this.serie = this.serieService.series$.getValue()
+        .find(serie => serie.id == this.serieId);
+    }
+    else {
+      // 2 récupérer les informations de la serie
+      this.serie = this.serieService.search$.getValue()
+        .find(serie => serie.id == this.serieId);
+    }
+
+
+    this.serieService
+      .getSerieInfo(this.serieId)
+      .subscribe( (data: any ) => {
+        this.urlYTSerie =
+          this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + data.results[0].key);
+      });
+
+
+
+
+
+  } // fin ngOnInit()
+
+
+  goToRootPage() {
+    this.serieService.search$.next([]);
+    this.router.navigate(['/series']);
+  }
+
+}
---
+++ src/app/list-serie/list-serie.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+
+import { SerieModel } from '../models/serie.model';
+import { SerieService } from '../Shared/services/serie.service';
+import { UserService } from '../Shared/services/user.service';
+import {BehaviorSubject} from "rxjs";
+
+
+
+@Component({
+  selector: 'app-list-serie',
+  templateUrl: './list-serie.component.html',
+  styleUrls: ['./list-serie.component.scss']
+})
+export class ListSerieComponent implements OnInit {
+
+  series: SerieModel[];
+  results: SerieModel[];
+
+  // là, on définit un observable sur lequel on pourra faire de l'asynchrone et éviter les apples successifs de subscribe
+  serieObs = new BehaviorSubject<Array<SerieModel>>([]);
+
+
+  page: number;
+  isLoading: boolean;
+
+
+  constructor(public serieService: SerieService , public userService: UserService) {
+    console.log('Je suis le constructor');
+
+  }
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    // request à l'API theMovie
+    this.serieService.getSeriesFromApi();
+
+    // on s'abonne à notre source de données movies$
+    this.serieService.series$.subscribe(
+      (data: SerieModel[]) => {
+        this.series = data;
+        this.isLoading = false;
+      });
+    this.serieObs = this.serieService.series$;
+    // on s'abonne à la source de données search$
+    this.serieService.search$.subscribe(data => this.results = data);
+
+
+  } // Fin ngOnInit()
+
+  printImageSrc(serie: SerieModel): string {
+    return 'https://image.tmdb.org/t/p/w500' + serie.image;
+  }
+
+
+  loadNextSeries() {
+    this.isLoading = true;
+    this.serieService.getNextSeriesFromApi();
+  }
+
+  getListOpacity() {
+    return this.isLoading ? 0.1 : 1;
+  }
+
+  searchSeries(searchText: string) {
+    console.log(searchText);
+    if (searchText.trim().length < 3) {
+      this.serieService.search$.next([]);
+    }
+    else {
+      this.serieService.searchSeriesFromApi(searchText);
+    }
+  }
+
+
+
+} // Fin class ListComponent
