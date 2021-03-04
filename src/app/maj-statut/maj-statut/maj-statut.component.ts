9de501c76567990526a9cc073bda9b52571d1ba2
CRECRE1965 <laurent.creton91@gmail.com>
2021-03-04 19:57:02 +0100

    Merge branch 'master' of https://github.com/OlivierDLMR/liv-app

---
+++ src/app/maj-statut/maj-statut/maj-statut.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Statut } from 'src/app/models/liste.model';
+
+
+@Component({
+  selector: 'app-maj-statut',
+  templateUrl: './maj-statut.component.html',
+  styleUrls: ['./maj-statut.component.scss']
+})
+export class MajStatutComponent implements OnInit {
+statut=Statut;
+statutLibelles:any;
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log ( "===> maj-statut-component : ");
+    console.log ( "     ===>ngOnInit : ");
+    console.log ("           statut", this.statut);
+    this.statutLibelles = Object.keys(this.statut).map(k =>this.statut[k] as string);
+    console.log ("          work : " ,this.statutLibelles);
+  }
+}
+
