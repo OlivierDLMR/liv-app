cafe9d0ef57e4880d4c980d0bc6250023ecb160a
Gwerrhod <abaillot.bnpparibas@gmail.com>
2021-03-09 09:25:45 +0100

    alex : ajout d'une nouvelle liste, début des travaux

--- src/app/listesuivis/listesuivis.component.ts
+++ src/app/listesuivis/listesuivis.component.ts
@@ -5,7 +5,7 @@ import {LoaderService} from '../Shared/services/loader.service';
 import {Utilisateur} from '../models/utilisateur.model';
 import {ListesNavBar, ListeSuivis, Preview, Statut, Suivi, TypePreview} from '../models/liste.model';
 import {SuivisService} from '../Shared/services/suivis.service';
-import { ActivatedRoute } from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 
 
 @Component({
@@ -15,23 +15,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ListesuivisComponent implements OnInit {
 
-  origineRating:string="maListe";
+  origineRating: string = "maListe";
 
   utilisateur: Utilisateur;
   listes: ListesNavBar;
   listeSuivis: ListeSuivis;
   suivi: Suivi;
   suiviId: number;
-  listeId:number;
-  listeName:string;
+  listeId: number;
+  listeName: string;
   isLoading: boolean;
- 
 
-  constructor(public suivisService: SuivisService, public userService: UserService, public loaderService: LoaderService, private route: ActivatedRoute) {
+
+  constructor(public suivisService: SuivisService,
+              public userService: UserService,
+              public loaderService: LoaderService,
+              private route: ActivatedRoute) {
   }
 
-  ngAfterViewChecked(): void{
-   
+  ngAfterViewChecked(): void {
+
     this.listeId = this.route.snapshot.params.id;
     this.listeName = this.route.snapshot.params.name;
   }
@@ -44,24 +47,24 @@ export class ListesuivisComponent implements OnInit {
         console.log(this.listeSuivis);
       }
     );
-        console.log(" ==> après subscribe" ,this.listeSuivis);
-       
+    console.log(' ==> après subscribe', this.listeSuivis);
+
   }
 
   printImageSrc(preview: Preview): string {
     return 'https://image.tmdb.org/t/p/w500/' + preview.image;
   }
 
-  getTypePreview(typePreview:TypePreview):string{
+  getTypePreview(typePreview: TypePreview): string {
     return TypePreview[typePreview];
   }
 
-  getStatut(typeStatut:Statut):string {
+  getStatut(typeStatut: Statut): string {
     return Statut[typeStatut];
   }
 
   isSerie(preview: Preview): boolean {
-    if (TypePreview[preview.typePreview] == TypePreview.SERIE){
+    if (TypePreview[preview.typePreview] == TypePreview.SERIE) {
       return true;
     }
     return false;
@@ -71,11 +74,12 @@ export class ListesuivisComponent implements OnInit {
   getListOpacity() {
     return this.isLoading ? 0.1 : 1;
   }
-  miseAJour(suivi:Suivi):void {
+
+  miseAJour(suivi: Suivi): void {
     console.log(" ===> miseA jour !!!")
-    console.log("              ",  suivi);
-   
+    console.log("              ", suivi);
+
   }
-  
+
 
 }
