20e886821b1bca1e3c051eb6b0b178525277fd55
OlivierDLMR <odlmr@yahoo.fr>
2021-03-18 18:13:32 +0100

    branch deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   TMDB_API_URL : 'https://api.themoviedb.org/3',
   TMDB_API_KEY : '03ff8dea5f45164c23521d42e141ea6d',
-  BE_API_URL : 'https://lostinview.herokuapp.com/',
+  BE_API_URL : 'https://lostinview.herokuapp.com',
 };
